b6a65d282f5be53edf7c1a29dfe0c13a6d382f4b
YashwanthSSBrigosha <yashwanth.s@brigosha.com>
2021-11-23 13:04:35 +0530

    first commit

---
+++ app/src/main/java/com/example/toastdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.toastdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void switchCat(View view){
+        ImageView image = (ImageView) findViewById(R.id.image);
+        image.setImageResource(R.drawable.cat2);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
